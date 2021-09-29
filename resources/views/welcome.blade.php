433b818ba66d47613de073651151d8abac9fba84
abayataceleste@gmail.com <niegodaisybelle@gmail.com>
2021-09-29 11:01:48 +0800

    change the word Laravel into HELLO WOLRD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HELLO WORLD
+                    HELLO WORLd
                 </div>
 
                 <div class="links">
